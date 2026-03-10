61df81e458a75e03827d6c8801cafc13119456b1
faizanque <faizanbuisness407@gmail.com>
2026-03-10 11:35:31 -0700

    Add files via upload

---
+++ lab 2/lab Task/lab 2 task 5.cpp
@@ -0,0 +1,24 @@
+// Name: muhammad faizan
+// Department: Electrical Engineering (Power)
+// Registration No: BF25NWELE0731
+// Section: A
+
+#include <iostream>   // library for input and output
+using namespace std;  // allows using cout directly
+
+int main()
+{
+    // printing name 10 times
+    cout << "muhammad faizan " << endl;
+    cout << "muhammad faizan " << endl;
+    cout << "muhammad faizan " << endl;
+    cout << "muhammad faizan " << endl;
+    cout << "muhammad faizan " << endl;
+    cout << "muhammad faizan " << endl;
+    cout << "muhammad faizan " << endl;
+    cout << "muhammad faizan " << endl;
+    cout << "muhammad faizan " << endl;
+    cout << "muhammad faizan " << endl;
+
+    return 0;  // program ends
+}
