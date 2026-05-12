4c77e2fe469679bee1718b0eef2a023c77cf4223
faizanque <faizanbuisness407@gmail.com>
2026-05-12 07:11:51 -0700

    Add files via upload

---
+++ lab 6/lab work/LAB_6 LabWork_4.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    for (int i = 1; i <= rows; i++) {        // controls rows
+        for (int j = 1; j <= i; j++) {       // controls stars
+            cout << "* ";
+        }
+        cout << endl;                        // move to next line
+    }
+
+    return 0;
+}
