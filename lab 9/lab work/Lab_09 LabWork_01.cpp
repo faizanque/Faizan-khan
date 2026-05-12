26ea6c8f28bed4a34708f586217fab6774d274bd
faizanque <faizanbuisness407@gmail.com>
2026-05-12 07:15:55 -0700

    Add files via upload

---
+++ lab 9/lab work/Lab_09 LabWork_01.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void greet()
+{
+    // Display greeting message
+    cout << "Welcome to C++ Programming!" << endl;
+}
+
+int main()
+{
+    // Function call
+    greet();
+
+    return 0;
+}
---
+++ lab 9/lab work/Lab_09 LabWork_02.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+// Function declaration
+void sayHello()
+{
+    // Display message
+    cout << "Hello, World!" << endl;
+}
+
+int main()
+{
+    // Function call
+    sayHello();
+
+    return 0;
+}
